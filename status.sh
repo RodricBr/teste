91a0e075f3901f7429a31c0430ac9aa2d2c07d66
Rodric <72948934+RodricBr@users.noreply.github.com>
2021-10-28 17:14:00 -0300

    Create status.sh

---
+++ status.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+ARQ="urls"
+HORA="$(date +'%d/%m/%y | %T')"
+echo "+----------[ Resultados $HORA ]----------+"
+
+while IFS= read -r line; do
+  WEBSITEHTTP=$(echo "http://$line")
+  WEBSITEHTTPS=$(echo "https://$line")
+  HTTP=$(echo | curl -s -w "%{http_code}\n" $WEBSITEHTTP -o /dev/null)
+  HTTPS=$(echo | curl -sL -w "%{http_code}\n" $WEBSITEHTTPS -o /dev/null)
+  domain=$(echo $line | awk -F[/:] '{print $1}')
+  IP=$(echo | nslookup $domain | awk '/^Address/{n++; if (n==2) {print $2}; if (n==3){print $2}; if (n==4){exit}}')
+  echo -e "\n+URL: $line\n+IP/s:\n$IP\n+STATUS: $HTTP(HTTP) e $HTTPS(HTTPS)"
+done < "$ARQ"
