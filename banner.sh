64f1dd997bff8db831eca1036e63642765c3f625
Rodric <72948934+RodricBr@users.noreply.github.com>
2021-10-28 17:03:34 -0300

    Update banner.sh

--- banner.sh
+++ banner.sh
@@ -2,7 +2,7 @@
 
 # Requer um arquivo chamado urls
 
-ARQ="urls"
+input="urls"
 HORA="$(date +'%d/%m/%y | %T')"
 
 echo -e "\n\n+----------[ Verificação Iniciada $HORA ]----------+"
@@ -10,21 +10,21 @@ echo -e "\n\n+----------[ Verificação Iniciada $HORA ]----------+"
 while IFS= read -r line; do
 
   echo -e "\n>>> URL: $line"
-  HTTP_=$(echo "http://$line")
-  HTTPS=_$(echo "https://$line")
-  RESHTTP=$(echo | curl -s -I $HTTP_ --connect-timeout 3 | grep -e Server:)
-  RESHTTPS=$(echo | curl -s -I $HTTPS_ --connect-timeout 3 | grep -e Server:)
-
-  if [ -z "$RESHTTP" ]; then
+  WEBSITEHTTP=$(echo "http://$line")
+  WEBSITEHTTPS=$(echo "https://$line")
+  RESULTSERVERHTTP=$(echo | curl -s -I $WEBSITEHTTP --connect-timeout 3 | grep -e Server:)
+  RESULTSERVERHTTPS=$(echo | curl -s -I $WEBSITEHTTPS --connect-timeout 3 | grep -e Server:)
+  #Resultados apresentados em tela e salvos em uma lista BANNER-LIST.txt
+  if [ -z "$RESULTSERVERHTTP" ]; then
     false
   else
-    echo -e "+ Resposta em HTTP:\n$RESHTTP"
+    echo -e "+ Resposta em HTTP:\n$RESULTSERVERHTTP"
   fi
 
-  if [ -z "$RESHTTPS" ]; then
+  if [ -z "$RESULTSERVERHTTPS" ]; then
     false
   else
-    echo -e "+ Resposta em HTTPS:\n$RESHTTPS"
+    echo -e "+ Resposta em HTTPS:\n$RESULTSERVERHTTPS"
   fi
 
-done < "$ARQ"
+done < "$input"
