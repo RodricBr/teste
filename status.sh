1ebf120e3534cce396258dfd303428a886189650
Rodric <72948934+RodricBr@users.noreply.github.com>
2021-10-28 17:14:58 -0300

    Update status.sh

--- status.sh
+++ status.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 
+# Requer um arquivo chamado urls
+# contendo as urls a serem analisadas
+
 ARQ="urls"
 HORA="$(date +'%d/%m/%y | %T')"
 echo "+----------[ Resultados $HORA ]----------+"
@@ -11,5 +14,6 @@ while IFS= read -r line; do
   HTTPS=$(echo | curl -sL -w "%{http_code}\n" $WEBSITEHTTPS -o /dev/null)
   domain=$(echo $line | awk -F[/:] '{print $1}')
   IP=$(echo | nslookup $domain | awk '/^Address/{n++; if (n==2) {print $2}; if (n==3){print $2}; if (n==4){exit}}')
+  
   echo -e "\n+URL: $line\n+IP/s:\n$IP\n+STATUS: $HTTP(HTTP) e $HTTPS(HTTPS)"
 done < "$ARQ"
