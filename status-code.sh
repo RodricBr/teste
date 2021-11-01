7aeabe3d0f488d2f27b9fb95fc4ac75bd21116fa
Rodric <72948934+RodricBr@users.noreply.github.com>
2021-11-01 14:38:10 -0300

    Update status-code.sh

--- status-code.sh
+++ status-code.sh
@@ -10,6 +10,7 @@ echo "+----------[ Resultados $HORA ]----------+"
 
 # Internal Field Separator(IFS) para evitar espaços em branco no output
 while IFS= read -r line; do
+
   WEBHTTP=$(echo "http://$line")
   WEBHTTPS=$(echo "https://$line")
   HTTP=$(echo | curl -s -w '%{http_code}\n' $WEBHTTP -o /dev/null)
@@ -19,5 +20,6 @@ while IFS= read -r line; do
   IP=$(echo | nslookup $DOMINIO | awk '/^Address/{n++; if (n==2) {print $2}; if (n==3){print $2}; if (n==4){exit}}')
   
   echo -e "\n+URL: $line\n+IP/s:\n$IP\n+STATUS: $HTTP(HTTP) e $HTTPS(HTTPS)"
+  
 # Lendo o conteúdo da variável das urls
 done < "$ARQ"
