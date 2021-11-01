9f122c344aba9197731188825a22c322f8ee57a4
Rodric <72948934+RodricBr@users.noreply.github.com>
2021-11-01 17:32:53 -0300

    Create subdomains.sh

---
+++ subdomains.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+if [[ -z "$*" ]]; then
+  echo -e "Uso: $0 <URL>"
+elif [[ ! -z "$*" ]]; then
+  touch $1-subs.txt && echo -e "\n---[ + ]--- Omnisint:\n" >> $1-subs.txt
+  OMN=$(curl "https://sonar.omnisint.io/subdomains/$1" -s -k | grep -oE "[a-zA-Z0-9._-]+\.$1" >> $1-subs.txt)
+  echo -e "\n---[ + ]--- Anubis:\n" >> $1-subs.txt
+  ANU=$(curl "https://jldc.me/anubis/subdomains/$1" -s -k | grep -oE "[a-zA-Z0-9._-]+\.$1" >> $1-subs.txt)
+  
+  # Tentando pegar o status code de cada linha do output file?? sou burro, deu beyblade, deu mt ruim
+  
+  #while IFS= read -r line; do
+  #  echo -e "OMNISI" | curl -s -w '%{http_code}\n' $OMN -o /dev/null >> $1-code.txt
+  #  echo -e "ANUBIS" | curl -sL -w '%{http_code}\n' $ANU -o /dev/null >> $1-code.txt
+  #done
+  #for i in "$1-subs.txt"; do
+  #  cat $1-subs.txt | curl $i -s -w -k '%{http_code}\n' -o /dev/null >> $1-status.txt
+  #done
+
+  if [[ -f "$1-subs.txt" ]]; then
+    echo -e "Arquivo com subdominios criado!"
+  else
+    echo -e "Nenhuma informação pode ser obtida!"
+  fi
+else
+  false
+fi
