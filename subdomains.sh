753da03a7029d7467f0eae9606dc7515bf524db8
Rodric <72948934+RodricBr@users.noreply.github.com>
2021-11-01 17:36:50 -0300

    Update subdomains.sh

--- subdomains.sh
+++ subdomains.sh
@@ -3,10 +3,14 @@
 if [[ -z "$*" ]]; then
   echo -e "Uso: $0 <URL>"
 elif [[ ! -z "$*" ]]; then
+  
+  HORA="$(date +'%d/%m/%y | %T')"
+  
   touch $1-subs.txt && echo -e "\n---[ + ]--- Omnisint:\n" >> $1-subs.txt
   OMN=$(curl "https://sonar.omnisint.io/subdomains/$1" -s -k | grep -oE "[a-zA-Z0-9._-]+\.$1" >> $1-subs.txt)
   echo -e "\n---[ + ]--- Anubis:\n" >> $1-subs.txt
   ANU=$(curl "https://jldc.me/anubis/subdomains/$1" -s -k | grep -oE "[a-zA-Z0-9._-]+\.$1" >> $1-subs.txt)
+  echo -e "\n+--------------- Scan terminado $HORA ---------------+\n" >> $1-subs.txt
   
   # Tentando pegar o status code de cada linha do output file?? sou burro, deu beyblade, deu mt ruim
   
